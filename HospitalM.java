ff63a5b83cb217bc254f4e0685c93ba807abcc4c
MyrtoCharala <t8180147@aueb.gr>
2019-12-31 11:54:40 +0200

    Add joptionpane message dialog in methods signUp and logIn

--- HospitalM.java
+++ HospitalM.java
@@ -9,172 +9,171 @@ public class HospitalM {
 	static String username;
 	static String phonenumber;
 	static double LimitInLiters;
-    static String password;
+	static String password;
 	static String Address;
 	public static ArrayList<Object> Hospitals = new ArrayList<Object>();
-    static Double[] bloodtypeLimit = new Double[8]; //a list with the minimum blood amount the current hospital needs
+	static Double[] bloodtypeLimit = new Double[8]; //a list with the minimum blood amount the current hospital needs
 	static String[]  bloodtype = {"O+", "O-", "A+", "A-" ,"B+" ,"B-" ,"AB+" ,"AB-"}; 
 	static Scanner input = new Scanner(System.in);
 	public static Object getList;
 	static String hospital_login;
 	static String password_login;
 	static String fullname; 
+	
 	public static boolean Answer() {
-		System.out.println("Are you sure this is the correct amount?Type yes or no.");
-		Scanner sc = new Scanner(System.in);
-		String answer = sc.nextLine();
-		if (answer.equals("yes")) {
+		int answer = JOptionPane.showConfirmDialog(null, "Are you sure this is the correct amount?");
+		if (answer == JOptionPane.YES_OPTION) {
 			return true;
 		}	
 		else {
 			return false;
 		}
 	}
-	public static double limitLiters(Scanner input,String b) {
+	
+	public static double limitLiters(String b) {
 		double temp;
 		do{
-			System.out.println("Please enter the limit amount of blood for blood type "+b+" :");
-			temp = input.nextDouble();
+			String t = JOptionPane.showInputDialog(null, "Please enter the limit amount of blood for blood type " + b + " :");
+			//doesn't perform a test if temp is not a number
+			temp = Double.valueOf(t);
 			if (temp < 0)
-				System.err.println("Please enter a positive number");
+				JOptionPane.showMessageDialog(null, "Please enter a positive number", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 		}while (temp<0);
 		return temp;
 			
 	}
-	public static double bloodLimit(Scanner input,int i) throws InputMismatchException{
+	public static double bloodLimit(int i) throws InputMismatchException{
 		 	String b = bloodtype[i];
-		 	boolean flag = false;
-			
+		 	boolean flag = true;			
 		 	do {
 				try {
-			 		double temp = limitLiters(input,b);
-					flag =  Answer();
-					if (flag == true)
+			 		double temp = limitLiters(b);
+					if (Answer() == true) {
 						LimitInLiters = temp;
-					if (flag == false) 
-						LimitInLiters = limitLiters(input,b);
-					break;	
-				}
-				catch(final InputMismatchException e2) {
-					System.err.println("The input has to be a number!");
-					final Scanner input1 = new Scanner(System.in);
-					double temp = limitLiters(input1,b);
-					flag =  Answer();
-					if (flag == false) { 
-						LimitInLiters =temp;
-						continue;	
-					}
-					if (flag == true) {
-						
+						flag = false;
+					} else {
+						LimitInLiters = limitLiters(b);
 						break;
 					}
-					
-					
-					
 				}
-			}while (flag == false);
-		 	
-		 	
-	
-		return LimitInLiters;
+				catch(final InputMismatchException e2) {
+					JOptionPane.showMessageDialog(null, "The input has to be a number!", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+					double temp = limitLiters(b);
+					if (Answer() == true) { 
+						LimitInLiters =temp;
+						flag = false;
+						continue;	
+					} else {	
+						break;
+					}				
+				}
+			}while (flag);
+			return LimitInLiters;
 	}
+
 	public static boolean correctPhonenumber(String phonenumber) throws HospitalPhoneNumberException{
 		boolean flag = false;
-		do {
+		while (flag == false) {
 			
-			if (phonenumber.length() <= 11) {
-				break;
-			}	
-			else {
+			if ((phonenumber.length() <= 11) && ((phonenumber.length() >= 10))) {
 				flag = true;
-				System.err.println("Please enter a valid phone number(up to 10 digits)");
+				break;
+			} else {
+				flag = true;
+				JOptionPane.showInputDialog(null, "Please enter a valid phone number(up to 10 digits)" , "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 			}
-		}while (flag);
+		}
 		return flag;
 	}
-	
 	
 	//sign up method
 	public static void signUp(){
 		
 		// Hospital's Name
-		boolean flag = true;
+		boo lean flag = true;
 		do {
 			try {	
-					System.out.println("Enter your hospital's name: ");
-					fullname = input.nextLine();
-					
+				String fullname = JOptionPane.showInputDialog(null,"Enter your hospital's name: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
 			}
-			catch (InputMismatchException e) {
-				System.err.println("Please enter a valid hospital name.");
+			catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "Please enter a valid hospital name.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 				flag = false;
-		}
+			}
 		}while (flag == false);
 		
+		//Hospital's username
+		boolean f = true;
+		do {
+			try {
+				String username = JOptionPane.showInputDialog(null,"Enter your hospital's username: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
+			}
+			catch (InputMismatchException e) {
+				JOptionPane.showMessageDialog(null, "Please enter a valid hospital username.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+				f = false;
+		}
+		}while (f == false);
+
 		// Hospital's phone number
 			flag = true;
-			while (flag == true) {
+			String phonenumber = JOptionPane.showInputDialog(null,"Enter your hospital's phone number: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
+			do {
 				try {
-					System.out.println("Enter your phone number.");
-					phonenumber = input.nextLine();
-					flag = correctPhonenumber(phonenumber);
+					int p = 0;
+					p=Integer.parseInt(phonenumber);
+					Hospital.correctPhonenumber(phonenumber);
+					flag = false;
 				}
-				catch (HospitalPhoneNumberException e1) {
-					System.err.println(e1.getMessage());
+				catch (NumberFormatException e1) {
+					JOptionPane.showMessageDialog(null, "Phone number must contain only numbers.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+					phonenumber = JOptionPane.showInputDialog(null,"Enter your hospital's phone number: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
 				}
-			}
-		
-			
-			
+			}while (flag);		
 		
 		// Hospital's address	
 		flag = true;
+		String Address = JOptionPane.showInputDialog(null,"Enter your hospital's address: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
 		do {
-			System.out.println("Enter your hospital's address");
-			Address = input.nextLine();
 			if (Address.matches("^.*(?=.{4,10})(?=.*\\d)(?=.*[a-zA-Z]).*$")) {
+				flag = false;
 				break;
 			} else {
-				flag = false;
-				System.err.println("Your address must contain both numbers and letters");
+				JOptionPane.showMessageDialog(null, "Your address must contain both letters and numbers", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+				Address = JOptionPane.showInputDialog(null,"Enter your hospital's address: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
 			}
-			
-		}while(flag == false);
-	
+		}while(flag);
 				
 		
 		
 		// Hospital's blood limit
 		for (int i = 0;i<=7; i++) {
-			bloodtypeLimit[i] = bloodLimit(input,i);
+			bloodtypeLimit[i] = bloodLimit(i);
 		}
 			
 		//password
-		flag = false;
+		flag = true;
 		do {
-			System.out.println("Enter a new password:");
-			Scanner input1 = new Scanner(System.in);
-			String password = input1.nextLine();
+			String password = JOptionPane.showInputDialog(null,"Enter your password: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
 			if (password.matches("^.*(?=.{4,10})(?=.*\\d)(?=.*[a-zA-Z]).*$")) {
+				flag = false;
 				break;
 			} else {
-				flag = false;
-				System.err.println("Your password must contain both numbers and letters");
+				JOptionPane.showMessageDialog(null, "Your password must contain both letters and numbers", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+
 			}
 			
-		}while(flag == false);
-	
+		}while(flag);
 			
 		
 		
 		Object CurrentHospitalData[] = new Object[5];
 		//a list that contains the current hospital's data that are about to be a part of the "Hospitals" list
 		
-		CurrentHospitalData[0] = username;
-		CurrentHospitalData[1] = phonenumber;
-		CurrentHospitalData[2] = Address;
-		CurrentHospitalData[3] = bloodtypeLimit;
-		CurrentHospitalData[4] = password;
+		CurrentHospitalData[0] = fullname;
+		CurrentHospitalData[1] = username;
+		CurrentHospitalData[2] = phonenumber;
+		CurrentHospitalData[3] = Address;
+		CurrentHospitalData[4] = bloodtypeLimit;
+		CurrentHospitalData[5] = password;
 		
 		Hospitals.add(CurrentHospitalData);
 		logIn(Hospitals);
@@ -183,22 +182,20 @@ public class HospitalM {
 		
 			boolean flag = true;
 			do {
-				System.out.println("Welcome!Please type your hospital's name. ");
-				hospital_login = input.nextLine();
+				String hospital_login = JOptionPane.showInputDialog(null,"Welcome!Please type your hospital's username. ", "LOG IN", JOptionPane.INFORMATION_MESSAGE);
 				flag = ((String) Hospitals).contains(hospital_login);
 				if (flag) {
 					boolean flag1 = true;
 					do {
-						System.out.println("Enter your password.");
-						password_login = input.nextLine();
+						String password_login = JOptionPane.showInputDialog(null,"Enter your password.", "LOG IN", JOptionPane.INFORMATION_MESSAGE);
 						flag1 = ((String) Hospitals).contains(password_login);
 						if (flag1 == false)
-							System.err.println("Wrong password!");
+							JOptionPane.showMessageDialog(null,"Wrong password!", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 					}while (flag1 == false);
 				}
 					
 				else {
-					System.err.println("This username is not registered!");
+					JOptionPane.showMessageDialog(null,"This username is not registered!", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 				}
 			}while (flag == false);
 	}

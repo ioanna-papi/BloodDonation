0c6b92cafb81ca22b14cdedd34e85e713f125a3d
MyrtoCharala <t8180147@aueb.gr>
2020-01-03 14:36:08 +0200

    Add joptionpane message dialog in questionnaire methos

--- BloodDonor.java
+++ BloodDonor.java
@@ -21,36 +21,20 @@ public class BloodDonor {
 	
 	final static Scanner input = new Scanner(System.in);	
 	
-	//custom exceptions for bloodtype&gender
+	//custom exception for gender
 	public static boolean correctGender(String gender) throws GenderException{
 		
 		if ((gender.toLowerCase().equals("male")) || (gender.toLowerCase().equals("female"))) {
 			return true;
-		}
-		else {
+		} else {
 			throw new GenderException("Please type male or female.");
 		}
-		
 	}
-	public static boolean correctBloodType(String bloodtype, String bloodtypes[]) throws BloodTypeException {
-		boolean flag = false;
-		int i=0;
-		while ((flag == false) && (i<=7)) {
-			if (bloodtype.equals(bloodtypes[i])) 
-				flag = true;
-			else 
-				i++;
-		}
-			if (flag == false) 
-				throw new BloodTypeException("Please enter a valid bloodtype.");
-				return flag;
-		}
-
 
 	/** This method lets users sign up to the application*/
 	public static void signUp()  {
 		//donor's full name
-		 boolean flag = true;
+		boolean flag = true;
                 do {
 			try{
 				  String fullname = JOptionPane.showInputDialog(null,"Enter your full name: ", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
@@ -85,7 +69,7 @@ public class BloodDonor {
 		//donor's bloodtype
 		flag = true;
 		while (flag) {
-		bloodtype = (String) JOptionPane.showInputDialog(null, "Blood Types", "Choose your blood type", JOptionPane.PLAIN_MESSAGE, null, bloodtypes, "O+" );
+		bloodtype = (String) JOptionPane.showInputDialog(null, "Choose your blood type", "SIGN UP", JOptionPane.PLAIN_MESSAGE, null, bloodtypes, "O+" );
 			try {
 				if (bloodtype.equals(null)) {
 					JOptionPane.showMessageDialog(null, "Please choose your blood type.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
@@ -114,17 +98,21 @@ public class BloodDonor {
 		
 		//donor's region
 		flag = true;
-		do {
+		Object[] possibilities = {"Attica","South Aegean Sea","North Aegean Sea","Central Greece","West Greece",
+			 "Ionian Islands","Thessaly","Peloponnese","Epirus","Eastern Macedonia and Thrace",
+			 "Central Macedonia","West Macedonia","Crete"};
+		while (flag) {
+			String region = (String)JOptionPane.showInputDialog(null, "Choose your region", "SIGN UP", JOptionPane.PLAIN_MESSAGE, null, possibilities, "Attica" );
 			try {
-				System.out.println("Enter your region:");
-				region = JOptionPane.showInputDialog(null,"Enter your region:", "SIGN UP", JOptionPane.INFORMATION_MESSAGE);
-				flag = false;
-			}
-			catch (InputMismatchException e4) {
-				JOptionPane.showMessageDialog(null, "Please enter a valid region", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
-			}
-				
-		}while(flag);			
+				if (bloodtype.equals(null)) {
+					JOptionPane.showMessageDialog(null, "Please choose your region.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+				} else {
+					flag = false;
+				}
+			} catch (NullPointerException e) {
+				JOptionPane.showMessageDialog(null, "Please choose your region.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+			} 
+		}
 		
 		//donor's password
 		flag = false;
@@ -176,16 +164,10 @@ public class BloodDonor {
 		}while (flag == false);
 	}
 		
-	
-public static void main(String args[]) {
-	signUp();
-	logIn(BloodDonors);
-}
 }
 	
 		 
 =======
-import java.util.Scanner;
 import java.io.*;
 import java.util.InputMismatchException;
 import java.sql.Connection;
@@ -198,76 +180,116 @@ public class BloodDonor {
 	
 	private static String[] answers = new String[51];
 	
-	public static void questionnaire() {
-		Scanner sc = new Scanner(System.in);
-		
 
+	/**
+	 * This method shows all the questions of the questionnaire*/
+	public static void questionnaire() {
+		String a;
 			try {
-			String url = "jdbc:mysql://localhost:3306/Donation?serverTimezone=UTC";
-			            String driver = "com.mysql.cj.jdbc.Driver";
-			            String userName = "root";
-			            String password = "";
-			Connection dbcon ;
-			dbcon = DriverManager.getConnection(url, userName, password);
-			Statement stmt = dbcon.createStatement();
-			int i = 0;
-			ResultSet rs = stmt.executeQuery("SELECT * FROM Questionnaire ");
-			while (rs.next()) {
-			String name = rs.getString("Q_id");
-			String r = rs.getString("Question");
-			System.out.println(name +" " + r);
-			String a = sc.nextLine();
-			answers[i] = a;
-			i++;
-			}
-			rs.close();
-			stmt.close();
+				String url = "jdbc:mysql://localhost:3306/Donation?serverTimezone=UTC";
+			        	    String driver = "com.mysql.cj.jdbc.Driver";
+			            	String userName = "root";
+			            	String password = "";
+				Connection dbcon ;
+				dbcon = DriverManager.getConnection(url, userName, password);
+				Statement stmt = dbcon.createStatement();
+				int i = 0;
+				ResultSet rs = stmt.executeQuery("SELECT * FROM Questionnaire ");
+				while (rs.next()) {
+					String qid = rs.getString("Q_id");
+					String r = rs.getString("Question");
+					if (qid.equals("1")) {
+						flag = false;
+                				do {
+                        				try {
+                                				a = JOptionPane.showInputDialog(null,  qid + ". " +  r, "QUESTIONNAIRE", JOptionPane.PLAIN_MESSAGE);
+                                				flag = correctGender(gender);
+                        				} catch (GenderException e1) {
+                                				JOptionPane.showMessageDialog(null, e1.getMessage(), "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+                        				}
+                				}while (flag == false);
+		
+					} else if ((qid.equals("2")) || (qid.equals("3")) || (qid.equals("4")) || 
+						(qid.equals("5")) || (qid.equals("6")) || (qid.equals("7")) || (qid.equals("8")) 
+						|| (qid.equals("9")) || (qid.equals("10")) || (qid.equals("11")) || 
+						(qid.equals("12")) || (qid.equals("14"))) {
+						a = JOptionPane.showInputDialog(null, qid + ". " + r, "QUESTIONNAIRE", JOptionPane.PLAIN_MESSAGE);
+					} else {
+						 a = JOptionPane.showConfirmDialog(null, qid + ". " + r, "QUESTIONNAIRE", JOptionPane.PLAIN_MESSAGE);
+					}
+					answers[i++] = a;
+				}
+				rs.close();
+				stmt.close();
 			} catch (Exception e) {
-			e.printStackTrace();
+				e.printStackTrace();
 			}
 		
 		}
 	
 
-
+		/**
+		 * This method takes the question that the user wants to change the answer to*/
 		public static void updateQuestionnaire() {
-			Scanner sc = new Scanner(System.in);
-			int a;
-			System.out.println("1.Sex ,male/female\n2.Last Name\n3.First Name\n 4.Father's Name\n5.Year of Birth\n6.Place of Birth\n7.Profession\n8.ID Number\n9.Address\n"
+			int a = 1;
+			JOptionPane.showMessageDialog(null,"1.Gender\n2.Last Name\n3.First Name\n 4.Father's Name\n5.Year of Birth\n6.Place of Birth\n7.Profession\n8.ID Number\n9.Address\n"
 		 		+ " 10.PostCode\n11.City\n12.Phone Number\n13.Have you ever gave blood before\n14.When was the last time you gave blood?\n"
-		 		+ "15.Have you ever been excluded from a blood donation?\n16.Do you have any dangerous profession or hobby?\n17.Have you had any health problems before?\n"
+		 		+ "15.excluded from a blood donation?\n16.dangerous profession or hobby?\n17.previous health problems?\n"
 		 		+ "18.jaundice or hepatitis\n19.syphilis\n20.malaria\n21.tuberculosis\n22.rheumatoid arthritis\n23.heart disease\n24.precardiac pan\n25.hypertension\n"
-		 		+ "26.convulsions(as an adult)\n27.fainting\n28.stomach aliments\n29.ulcer\n30.kidney disease\n31.diabetes\n32.anemia\n33.allergy\n"
-		 		+ "34.What are you allergic to?\n35.contagious disease in your environment?\n36.drug intake?\n37.taken aspirin the last 5 days?\n"
-		 		+ "38.born or lived or traveled aboard?\n39.lost weight, had fever or swollen tonsils?\n40.have you ever had a cornea or scar implant in your eye?\n"
-		 		+ "41.intake of growth hormone extract?\n42.tooth extraction or treatment the past week?\n43.vaccines the past week?\n"
-		 		+ "44.surgery or medical examinations the past year?\n45.transfusion of blood or blood producers?\n46.tattoo or ear piercing or acupuncture\n"
-		 		+ "47.piercedby a syringe needle\n48.any skin wounds or sores of yours or mucous membrane of your mouth came in contact with foreign blood?\n"
-		 		+ "49.were you pregnant the past year?\n50.have you heard that your family is in danger of developing the Creutzfeldt-Jakob disease?\n");
-			a = 1;
-		 System.out.println("Which answer you want to change? press the number of the question if you want to exit press 0");
-		 a = sc.nextInt();
-		 changeQuestion(a);
-		 while (a != 0 ) {
-		 try {
-		     if (a >= 1 && a <= 51) {
-			 System.out.println("If you want to change another question press the number of the question or else press 0");
-			 a = sc.nextInt();
-			 changeQuestion(a);
-		     }else { System.out.println("You must type the number of the question");
-		     }
-		 } catch (InputMismatchException e) { 
-			 }
-         }
-	 
-	}
+		 		+ "26.convulsions(as an adult)\n27.fainting\n28.stomach aliments\n29.ulcer\n30.other surgeries\n31.kidney diseasea\n32.diabetes\n33.allergies\n"
+		 		+ "34.anemia\n35.other diseasess\n36.contagious diseases in your envirnment?\n37.taken medicine?\n"
+		 		+ "38.take aspirin?\n39.born or lived or traveled aboard?\n40.lost weight, had fever or swollen tonsils?\n"
+		 		+ "41.cornea or scar implant in your eye?\n42.Creutzfeldt-Jakob disease?\n43.growth hormones?\n"
+		 		+ "44. tooth extraction or treatment the past week?\n45.vaccines the past week?\n46.surgery or medical examinations the past year?\n"
+				+"47.transfusion of blood or blood producers?\n48.tattoo or ear piercing or acupuncture?\n"
+		 		+ "49.pierced by a syringe needle\n50.any skin wounds or scratches that came in contact with foreign blood?\n"
+		 		+ "51.were you pregnant the past year?\n");
+			boolean g = true;
+			do {
+				try {
+					a = Integer.parseInt(JOptionPane.showInputDialog("If you want to change a question press the number of the question or else press 0"));
+	            	 		if (a >= 1 && a <= 51) {
+	            		 		g = false;
+	            		 		changeQuestion (a);
+						updateQuestionnaire();
+	            	 		} else if (a == 0) {
+	            		 		g = true;
+	            		 		break;
+	            		 	} else {
+	            		 		JOptionPane.showMessageDialog(null, "Please enter a valid question number","ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+	            	 		}
+	           	  	} catch (NumberFormatException e) {
+	                     		JOptionPane.showMessageDialog(null, "Please enter a number","ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+	                     		g = true;
+	             		}
+			} while (g);	 
+		}
 		
-		public static void changeQuestion(int a) {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Update your answer");
-			String a2 = sc.nextLine();
-			// χειρισμό εξαιρέσεων (try-catch)
+		/**
+		 * This method changes the answer of the given question*/
+		public static void changeQuestion(int qid) {
+			boolean flag;
+			String a2;
+			 if (qid.equals("1")) {
+                         flag = false;
+                         	do {
+                                	try {
+                                        	a2 = JOptionPane.showInputDialog(null,  qid + ". " +  r, "QUESTIONNAIRE", JOptionPane.PLAIN_MESSAGE);
+                                                flag = correctGender(gender);
+                                        } catch (GenderException e1) {
+                                        	JOptionPane.showMessageDialog(null, e1.getMessage(), "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+                                        }
+                                }while (flag == false);
+                         } else if ((qid.equals("2")) || (qid.equals("3")) || (qid.equals("4")) ||
+                         	(qid.equals("5")) || (qid.equals("6")) || (qid.equals("7")) || (qid.equals("8"))
+                                || (qid.equals("9")) || (qid.equals("10")) || (qid.equals("11")) ||
+                                (qid.equals("12")) || (qid.equals("14"))) {
+                                	a2 = JOptionPane.showInputDialog(null, qid + ". " + r, "QUESTIONNAIRE", JOptionPane.PLAIN_MESSAGE);
+                         } else {
+                         	a2 = JOptionPane.showConfirmDialog(null, qid + ". " + r, "QUESTIONNAIRE", JOptionPane.PLAIN_MESSAGE);
+                         }
 			answers[a-1] = a2;
+			return;
 		}
 		
 }

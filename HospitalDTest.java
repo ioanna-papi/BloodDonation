d014d77489e6808eadc2f4ac9c00c713c6697ea3
ChatzistDimi <dimichatzist@gmail.com>
2020-01-09 18:56:19 +0200

    Format JUnit Test of method bloodBankStock of class Hospital

--- HospitalDTest.java
+++ HospitalDTest.java
@@ -1,14 +1,12 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class HospitalDTest {
 
 	@Test
 	public void test() {
-		HospitalD hd = new HospitalD();
-		hd.bloodBankStock();
+		HospitalD hD = new HospitalD();
+		hD.bloodBankStock();
 	}
-
 }
 

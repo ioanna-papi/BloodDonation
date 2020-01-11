01048091d0523e5cdbbbd4c13f4799b4cb3dc958
MyrtoCharala <t8180147@aueb.gr>
2020-01-11 23:31:42 +0200

    Change parametres of shortageOfBlood method(remove region)

--- MessagesE.java
+++ MessagesE.java
@@ -11,10 +11,23 @@ public class Messages {
 	
 	/**
 	 * This method informs a hospital about it's shortage of blood in a specific blood type
-	 * @param region the hospital's region
 	 * @param bloodtype the specific blood type which is lacking in the specific hospital
 	 * @param username the hospital's username*/
-	public void shortageOfBlood(String region, String bloodtype, String username) {//must have parametre username and region of hospital!!
+	public void shortageOfBlood(String bloodtype, String username) {
+		String region = null;
+                try {
+                        Messages.connect();
+                        Connection dcon = null;
+                        Statement stmt = dbcon.createStatement(); //incorrect syntax of values
+                        ResultSet rs = stmt.executeQuery("SELECT Region, Username FROM Hospital WHERE Username ='" + username +"'");
+                        while (rs.next()) {
+                                region = rs.getString("Region");
+                        }
+                        stmt.close();
+                        Messages.connect().close();
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }
 		JOptionPane.showMessageDialog(null, "SHORTAGE OF BLOOD TYPE " + bloodtype, "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 		boolean flag = true;
 		while(flag) {
@@ -26,11 +39,11 @@ public class Messages {
 			if (choice == 0) {
 				flag = false;
 				Messages mes = new Messages();
-	 		       	mes.bloodBorrow(region, bloodtype, username); 
+	 		       	mes.bloodBorrow(region,bloodtype, username); 
 			} else if (choice == 1) {
 				flag = false;
 				Hospital h = new Hospital();
-	        		h.makeDonationDay();
+	        		h.makeDonationDay(username);
 			} else {
 				JOptionPane.showMessageDialog(null, "You have to choose one of the above!", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
 			}
@@ -39,36 +52,29 @@ public class Messages {
 	}
 	
 	/**sends message to hospitals in order to borrow blood to a hospital in the same region
-	 * @param region the region the specific hospital belongs to
+	 * @param region is the region the hospital belongs to
 	 * @param bloodtype the type of blood the hospital is lacking
 	 * @param username the hospital's username*/
 	public void bloodBorrow(String region, String bloodtype, String username) {
+		String r = null; 
 		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-		}
-		String url = "jdbc:sqlserver://sqlserver.dmst.aueb.gr:1433;"
-		    		+ "databaseName=DB20;user=G520;password=94we99494";
-			
-		Connection dbcon ;
-		try {
-			dbcon = DriverManager.getConnection(url);
-			Statement stmt = dbcon.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT Region, Username, H_name FROM Hospital  WHERE region = '" + region+ "' and Username='" +username +"'");
+                        Messages.connect();
+                        Connection dcon = null;
+                        Statement stmt = dbcon.createStatement(); //incorrect syntax of values
+                        ResultSet rs = stmt.executeQuery("SELECT H_name, Region, Username FROM Hospital WHERE Username ='" + username +"'");
 			while (rs.next()) {
 				String name = rs.getString("H_name");
-				String r = rs.getString("Region");
-					if (r.equals(region)) {
-						JOptionPane.showMessageDialog(null, name + " has blood type " + bloodtype+ " shortage.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
-					}
+				r = rs.getString("Region");
+				if (r.equals(region)) {
+					//display message to hospitals
+					JOptionPane.showMessageDialog(null, name + " has blood type " + bloodtype+ " shortage.", "ALERT MESSAGE", JOptionPane.WARNING_MESSAGE);
+				}
 			}
-		rs.close();
-		stmt.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+                        stmt.close();
+                        Messages.connect().close();
+                } catch (Exception e) {
+                        e.printStackTrace();
+                }	
 	}
 }
 

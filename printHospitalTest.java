ecd2ed2ae31b8339a761200cf1d6b6976f1b3725
ioanna-papi <57221590+ioanna-papi@users.noreply.github.com>
2020-01-14 05:37:25 +0200

    create the tester of printHospitalMenu

---
+++ printHospitalTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class printHospitalTest {
+
+	@Test
+	void test() {
+		HomeMenu.printHospitalMenu();
+	}
+
+}

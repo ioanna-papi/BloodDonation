937f2b6f5446235d096cce82dc978c7457901b1e
MyrtoCharala <t8180147@aueb.gr>
2020-01-14 12:47:25 +0200

    Add message in donationDay method of class Messages

--- Messages.java
+++ Messages.java
@@ -28,11 +28,14 @@ public class Messages{
 	 * @param username is the hospital's username*/
 	public static void donationDay(String date, String username){
 		String region = null;
+		String name = null;
 		try {
-			ResultSet rs = Messages.connect().executeQuery("SELECT Region, Username FROM Hospital WHERE Username = '" + username+ "'");
+			ResultSet rs = Messages.connect().executeQuery("SELECT Region,H_name, Username FROM Hospital WHERE Username = '" + username+ "'");
 			while (rs.next()) {
+				name = rs.getString("H_name");
 				region = rs.getString("Region");
 				//dispaly message to volunteers
+				JOptionPane.showMessageDialog(null, name + "hospital has created a new donation day at " + date );
 			}
 			rs.close();		
 			Messages.connect().close();
@@ -51,56 +54,56 @@ public class Messages{
 			System.out.print("ClassNotFoundException: ");
 			System.out.println(e.getMessage());
 		}
-		do(;;){
-		try {
-			ResultSet rs = Messages.connect().executeQuery("SELECT * FROM DonationDays");
-			ResultSet RS = Messages.connect().executeQuery("SELECT * FROM BloodDonor");
-			SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-			Date date = new Date();
-			int d ,m;
-			while ((rs.next())) {
-				Date d_date = rs.getDate("D_Date");
-				String strDate = formatter.format(d_date);
-				String strDateDay = strDate.substring(8);//from current date get day
-				String strDateMonth = strDate.substring(5,6);//from current date get month
-				String strDateYear = strDate.substring(0,3);//from current date get year
-				d = Integer.parseInt(strDateDay);
-				m = Integer.parseInt(strDateMonth);
-				//find date which is five days before Blood Donation day
-				if (d-5 <= 0) {
-					if(m == 2 || m == 4 || m == 6 || m == 9 || m == 11) {
-						d = 30 + (d-5);
-						m -= 1;
-					} else {
-						d = 31 + (d-5);
-						if(m == 1){
-							m = 12;
-						}else{
+		for(;;){
+			try {
+				ResultSet rs = Messages.connect().executeQuery("SELECT * FROM DonationDays");
+				ResultSet RS = Messages.connect().executeQuery("SELECT * FROM BloodDonor");
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+				Date date = new Date();
+				int d ,m;
+				while ((rs.next())) {
+					Date d_date = rs.getDate("D_Date");
+					String strDate = formatter.format(d_date);
+					String strDateDay = strDate.substring(8);//from current date get day
+					String strDateMonth = strDate.substring(5,6);//from current date get month
+					String strDateYear = strDate.substring(0,3);//from current date get year
+					d = Integer.parseInt(strDateDay);
+					m = Integer.parseInt(strDateMonth);
+					//find date which is five days before Blood Donation day
+					if (d-5 <= 0) {
+						if(m == 2 || m == 4 || m == 6 || m == 9 || m == 11) {
+							d = 30 + (d-5);
 							m -= 1;
+						} else {
+							d = 31 + (d-5);
+							if(m == 1){
+								m = 12;
+							}else{
+								m -= 1;
+							}
 						}
 					}
+					String strd = Integer.toString(d); 
+					String strm = Integer.toString(m);
+					if (d < 10) {
+						 strd = "0" + d;	 
+					}
+					if (m < 10) {
+						 strm = "0" + m;
+					}   
+					String messageDate = strDateYear + "-" + strm + "-" + strd;
+					//if today is five days before the Blood Donation day display message to volunteers 
+					if (formatter.format(date).equals(messageDate)) {
+						String day = rs.getString("D_Day");
+						JOptionPane.showMessageDialog(null, day, strDate, JOptionPane.INFORMATION_MESSAGE);					
+					}
+					rs.close();
+					Messages.connect().close();
 				}
-				String strd = Integer.toString(d); 
-				String strm = Integer.toString(m);
-				if (d < 10) {
-					 strd = "0" + d;	 
-				}
-				if (m < 10) {
-					 strm = "0" + m;
-				}   
-				String messageDate = strDateYear + "-" + strm + "-" + strd;
-				//if today is five days before the Blood Donation day display message to volunteers 
-				if (formatter.format(date).equals(messageDate)) {
-					String day = rs.getString("D_Day");
-					JOptionPane.showMessageDialog(null, day, strDate, JOptionPane.INFORMATION_MESSAGE);					
-				}
-				rs.close();
-				Messages.connect().close();
+				break;
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			break;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 		}
 		return;	
 	}
@@ -110,23 +113,23 @@ public class Messages{
 	 * @rparam username is the hospital' username */
 	public static String getRegion(String username) {
 		String region = null;
-        try {
-                Messages.connect();
-                Connection dbcon = null;
-                Statement stmt = dbcon.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT Region, Username FROM Hospital WHERE Username ='" + username +"'");
-                while (rs.next()) {
-                        region = rs.getString("Region");
-                }
-                stmt.close();
-                Messages.connect().close();
-        } catch (Exception e) {
-                e.printStackTrace();
-        }
-        return region;
+        	try {
+                	Messages.connect();
+                	Connection dbcon = null;
+                	Statement stmt = dbcon.createStatement();
+                	ResultSet rs = stmt.executeQuery("SELECT Region, Username FROM Hospital WHERE Username ='" + username +"'");
+                	while (rs.next()) {
+                	        region = rs.getString("Region");
+                	}
+                	stmt.close();
+                	Messages.connect().close();
+        	} catch (Exception e) {
+                	e.printStackTrace();
+        	}
+        	return region;
 	}
 
-		/**
+	/**
 	 * This method informs a hospital about it's shortage of blood in a specific blood type
 	 * @param bloodtype the specific blood type which is lacking in the specific hospital
 	 * @param username the hospital's username*/
@@ -151,7 +154,7 @@ public class Messages{
 		return;
 	}
 
-	/**sends message to hospitals in order to borrow blood to a hospital in the same region
+	/**This method displays message to hospitals in order to borrow blood to a hospital in the same region
 	 * @param region is the region the hospital belongs to
 	 * @param bloodtype the type of blood the hospital is lacking
 	 * @param username the hospital's username*/
